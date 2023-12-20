dcc049faf708ffd397c29a13a966f3ab46f86b7c
Prabhat-kr-Tiwari <sonukbc6@gmail.com>
2023-12-20 13:28:10 +0530

    added the on back pressed

--- app/src/main/java/com/example/popup/SecondActivity.kt
+++ app/src/main/java/com/example/popup/SecondActivity.kt
@@ -4,13 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.OnBackPressedCallback
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class SecondActivity : AppCompatActivity() {
     //create button variable of type button
     private lateinit var button: Button
+    //handling back button clicl
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            //showing dialog and then closing the application..
+            showDialog()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        // adding onbackpressed callback listener.
+        onBackPressedDispatcher.addCallback(this,onBackPressedCallback)
+
         //intializing the button
         button=findViewById<Button>(R.id.btn_send)
         //adding the setOnCLick Listener
@@ -25,5 +38,14 @@ class SecondActivity : AppCompatActivity() {
             startActivity(shareIntent)
         }
 
+    }
+    private fun showDialog(){
+        MaterialAlertDialogBuilder(this).apply {
+            setTitle("Are you sure?")
+            setMessage("want to close the application ?")
+            setPositiveButton("Yes") { _, _ -> finishAffinity() }
+            setNegativeButton("No", null)
+            show()
+        }
     }
 }
